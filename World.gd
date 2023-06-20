fcc0224043f6f08347e46f887138468c66304cad
Advik49 <advik.adhiyaman@gmail.com>
2023-06-19 23:17:57 -0400

    i

--- World.gd
+++ World.gd
@@ -3,13 +3,16 @@ extends Node
 @onready var main_menu = $CanvasLayer/MainMenu
 @onready var address_entry = $CanvasLayer/MainMenu/MarginContainer/VBoxContainer/AddressEntry
 
+const PORT = 9999
+var enet_peer = ENetMultiplayerPeer.new()
+
 func _unhandled_input(event):
 	if Input.is_action_just_pressed("quit"):
 		get_tree().quit()
 
 
 func _on_host_button_pressed():
-	pass # Replace with function body.
+	main_menu.hide()
 
 
 func _on_join_button_pressed():
